fde6d0b037eb132f9cb88fc55298553cda37e45f
PivotAlfa <backpacker0220@gmail.com>
2023-02-07 22:52:59 +0900

    Al

---
+++ app.py
@@ -0,0 +1,43 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+import plotly.express as px
+from streamlit_lottie import st_lottie
+from sklearn.metrics import confusion_matrix
+
+
+import mysql.connector
+from mysql.connector import Error
+
+import pandas as pd
+#try:
+#    mydb = connection.connect(host="127.0.0.1", database = 'first_project',user="root", passwd="backpacker0220!!",use_pure=True)
+#    query = "Select * from watch_me;"
+#    result_dataFrame = pd.read_sql(query,mydb)
+#    mydb.close() #close the connection
+#except Exception as e:
+#    mydb.close()
+
+import pandas as pd
+st.sidebar.slider("Aga",min_value=8, max_value=80)
+
+db_connection = mysql.connector.connect(host='127.0.0.1',
+    database='for_imp', 
+    user='root', 
+    password='backpacker0220!!')
+
+db_cursor = db_connection.cursor()
+db_cursor.execute("Select * from watch_me")   
+result = db_cursor.fetchall()
+df = pd.DataFrame(result, columns=db_cursor.column_names)
+
+db_connection.commit()
+
+
+st.write(df)
+st.write('Alfabeta')
+st.write(df['brand'].unique())
+text_in = st.text_input("Please tell Brand")
+st.success("Grrean")
